fbff617d25a64398fe6bfa09995d9e447b1decb7
Whatruska <vitdub@inbox.ru>
2019-10-18 11:20:37 +0300

    new lesson

---
+++ src/Lesson_6/FirstTask.java
@@ -0,0 +1,11 @@
+package Lesson_6;
+
+import java.util.Scanner;
+
+public class SecondTask {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i = 0;
+
+    }
+}
---
+++ src/Lesson_6/SecondTask.java
@@ -0,0 +1,30 @@
+package Lesson_6;
+
+import java.util.Scanner;
+
+public class FirstTask {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i = 0;
+
+        System.out.print("Введите x : ");
+        float x = scanner.nextFloat();
+
+        System.out.print("Введите y : ");
+        float y = scanner.nextFloat();
+
+        boolean bool1 = y * y <= 1 - (x * x);
+        boolean bool2 = y >= 0 & x <= 0;
+        boolean bool3 = y <= 0 & x >= 0;
+
+        if (bool1 & (bool2 || bool3)){
+            System.out.println("(" + x + " ; " + y + ") входит в область");
+        } else {
+            System.out.println("(" + x + " ; " + y + ") НЕ входит в область");
+        }
+
+        //y^2 = 1 - x^2
+
+
+    }
+}
