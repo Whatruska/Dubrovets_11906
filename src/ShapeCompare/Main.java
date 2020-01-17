07e6d785029f3cd52d38cf25e3f0a20dbac433e8
Whatruska <vitdub@inbox.ru>
2020-01-17 22:14:17 +0300

    update

--- src/ShapeCompare/Main.java
+++ src/ShapeCompare/Main.java
@@ -2,13 +2,22 @@ package ShapeCompare;
 
 public class Main {
     public static void main(String[] args) {
-        NShape shape = new NShape(
-                new Point(5,10),
-                new Point(1,2)
-        );
-        System.out.println(shape.isIntersects(new NShape(
-                new Point(4,3),
-                new Point(14,7)
-        )));
+        System.out.println(Math.E);
+
+        double E = 1 * Math.pow(10, -24);
+
+        double curr = 0;
+        double last = 0;
+
+        //Считаем lim n -> inf (1 + 1/n)^2, т.е Неперово число (e = 2,7....)
+        for (long i = 0; i < Long.MAX_VALUE; i++){
+            last = curr;
+            curr = Math.pow((1.0 + 1.0/(i + 1)),i + 1);
+            if (Math.abs(curr - last) < E && i > 1){
+                break;
+            }
+        }
+
+        System.out.println(last);
     }
 }
